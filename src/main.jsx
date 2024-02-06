e0141bf6262212e0c655a2356792cc499247032b
Freemasoid <freemasoid@icloud.com>
2024-02-06 18:29:00 +0100

    chore: reducer update

--- src/main.jsx
+++ src/main.jsx
@@ -5,18 +5,21 @@ import { ProductsProvider } from "./context/products_context.jsx";
 import { FilterProvider } from "./context/filter_context.jsx";
 import { CartProvider } from "./context/cart_context.jsx";
 import { UserProvider } from "./context/user_context.jsx";
+import { OrderProvider } from "./context/order_context.jsx";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <UserProvider>
-    <ProductsProvider>
-      <FilterProvider>
-        <CartProvider>
-          <ToastContainer position="top-center" limit={2} />
-          <App />
-        </CartProvider>
-      </FilterProvider>
-    </ProductsProvider>
+    <OrderProvider>
+      <ProductsProvider>
+        <FilterProvider>
+          <CartProvider>
+            <ToastContainer position="top-center" limit={2} />
+            <App />
+          </CartProvider>
+        </FilterProvider>
+      </ProductsProvider>
+    </OrderProvider>
   </UserProvider>
 );
