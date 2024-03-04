c25be72621d4d2c0ab4c8ac6aa6a9d97a5f637dd
Freemasoid <freemasoid@icloud.com>
2024-03-05 00:02:47 +0100

    feat: order modal contents

---
+++ src/components/OrderItem.jsx
@@ -0,0 +1,135 @@
+/* eslint-disable react/prop-types */
+import styled from "styled-components";
+import { formatPrice } from "../utils/helpers.js";
+
+const OrderItem = ({ name, color, price, amount }) => {
+  return (
+    <Wrapper>
+      <h5>x {amount}</h5>
+      <h5 className="name">{name}</h5>
+      <p className="color">
+        color: <span style={{ background: color }}></span>
+      </p>
+      <h5 className="price">{formatPrice(price)}</h5>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.article`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 0.5rem;
+  padding: 0.7rem;
+  border-bottom: 0.01rem solid var(--clr-grey-4);
+
+  .color {
+    color: var(--clr-grey-5);
+    font-size: 0.75rem;
+    letter-spacing: var(--spacing);
+    text-transform: capitalize;
+    margin-bottom: 0;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    span {
+      display: inline-block;
+      width: 0.5rem;
+      height: 0.5rem;
+      background: red;
+      margin-left: 0.5rem;
+      border-radius: var(--radius);
+    }
+  }
+  .price-small {
+    color: var(--clr-primary-5);
+  }
+  .amount-btns {
+    width: 75px;
+    button {
+      width: 1rem;
+      height: 0.5rem;
+      font-size: 0.75rem;
+    }
+    h2 {
+      font-size: 1rem;
+    }
+  }
+  .remove-btn {
+    color: var(--clr-white);
+    background: transparent;
+    border: transparent;
+    letter-spacing: var(--spacing);
+    background: var(--clr-primary-5);
+    width: 1.5rem;
+    height: 1.5rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: var(--radius);
+    font-size: 0.75rem;
+    cursor: pointer;
+  }
+  @media (min-width: 776px) {
+    display: grid;
+    grid-template-columns: 200px auto auto;
+    grid-template-rows: 75px;
+    gap: 3rem 1rem;
+    justify-items: center;
+    align-items: center;
+
+    .subtotal {
+      display: block;
+      margin-bottom: 0;
+      color: var(--clr-grey-5);
+      font-weight: 400;
+      font-size: 1rem;
+    }
+    .price-small {
+      display: none;
+    }
+    .price {
+      display: block;
+      font-size: 1rem;
+      color: var(--clr-primary-5);
+      font-weight: 400;
+    }
+    .name {
+      font-size: 0.85rem;
+    }
+    .color {
+      font-size: 0.85rem;
+      span {
+        width: 0.75rem;
+        height: 0.75rem;
+      }
+    }
+    grid-template-columns: 1fr 1fr 1fr 1fr auto;
+    align-items: center;
+    grid-template-rows: 75px;
+    img {
+      height: 100%;
+    }
+    .title {
+      height: 100%;
+      display: grid;
+      grid-template-columns: 100px 200px;
+      align-items: center;
+      gap: 1rem;
+      text-align: left;
+    }
+    .amount-btns {
+      width: 100px;
+      button {
+        width: 1.5rem;
+        height: 1rem;
+        font-size: 1rem;
+      }
+      h2 {
+        font-size: 1.5rem;
+      }
+    }
+  }
+`;
+
+export default OrderItem;
